218fd2b7e419a544a2ba92bf5af5ecfef1d82db3
Rishabh Verma <rishabh.verma11998@gmail.com>
2018-06-18 23:05:49 +0530

    Feat: Add code for registering a service worker

--- js/main.js
+++ js/main.js
@@ -10,6 +10,13 @@ var markers = [];
 document.addEventListener('DOMContentLoaded', (event) => {
   fetchNeighborhoods();
   fetchCuisines();
+  
+  // Register a service worker
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+    .then((resp) => console.log('registered'))
+    .catch(() => console.log('This Browser does not support service worker'));
+  }
 });
 
 /**
