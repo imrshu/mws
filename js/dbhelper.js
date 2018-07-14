13e45776dcf91c825bc7834884f059c658956d75
Rishabh Verma <rishabh.verma11998@gmail.com>
2018-07-14 22:38:27 +0530

    Fix: Change google maps chart map marker icon http request to https

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -153,14 +153,14 @@ class DBHelper {
     return (`/img/${restaurant.photograph}`);
   }
 
-  /** 
+  /**
    * This function takes in a COLOR, and then creates a new marker
    * icon of that color. The icon will be 21 px wide by 34 high, have an origin
    * of 0, 0 and be anchored at 10, 34).
    */
   static makeMarkerIcon(markerColor) {
     const markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+      'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
       '|40|_|%E2%80%A2',
       new google.maps.Size(21, 34),
       new google.maps.Point(0, 0),
