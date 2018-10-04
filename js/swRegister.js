9ff7e0db254cb4dec24a335855b957322a858339
Rishabh Verma <rishabh.verma11998@gmail.com>
2018-10-04 23:00:57 +0530

    Refactor: Testing web app without sw

--- js/swRegister.js
+++ js/swRegister.js
@@ -1,6 +1,6 @@
 // Register a service worker
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js')
-    .then((resp) => console.log('registered'))
-    .catch(() => console.log('This Browser does not support service worker'));
-}
+// if (navigator.serviceWorker) {
+//     navigator.serviceWorker.register('/sw.js')
+//     .then((resp) => console.log('registered'))
+//     .catch(() => console.log('This Browser does not support service worker'));
+// }
